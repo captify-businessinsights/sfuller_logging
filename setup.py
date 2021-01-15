f15147cbd8fc09adbb681029b821dd68ffe0455f
captify-sfuller <52924007+captify-sfuller@users.noreply.github.com>
2021-01-15 11:46:45 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sfuller_logging',
-      version='0.8',
+      version='1.0',
       description='logging shortcut',
       packages=['sfuller_logging'],
       author_email='sebastian.fuller@captify.co.uk',
